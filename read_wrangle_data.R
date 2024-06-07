4802bf4e55e09310abd308e2e3aefdd57b57a457
stewaw01 <stewart.wilson@phs.scot>
2024-06-07 14:51:27 +0100

    line/area chart

--- read_wrangle_data.R
+++ read_wrangle_data.R
@@ -30,18 +30,45 @@ data <- read_excel(here("data", "Waiting Monthly [New Time-Bands] Snapshot ---Ex
          "156+ weeks",
          "208+ weeks"))
 
-scotland <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator %in% c("52+ weeks", 
-                                                                                   "104+ weeks",
-                                                                                   "156+ weeks",
-                                                                                   "208+ weeks"
-                                                                                   )) %>% 
+scotland1 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator %in% c("52+ weeks")) %>% 
   filter(Specialty == "All AHP MSK Specialties")
 
+scotland2 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator %in% c("104+ weeks")) %>% 
+  filter(Specialty == "All AHP MSK Specialties")
 
-scotland %>%
-  plot_ly() %>% 
-  add_bars( 
-           x = ~`Month end`,
-           y = ~Value, color = ~Indicator,
-           colors = phs_colors(c("phs-blue", "phs-rust", "phs-purple", "phs-green")),
-           stroke = I("black"))
+lines1 <- plot_ly() %>%
+  add_lines(
+    x = ~scotland1$`Month end`,
+    y = ~scotland1$Value,
+    color = ~scotland1$Indicator,
+    colors = phs_colors(c("phs-green", "phs-rust"))
+  )
+lines1 <- lines1 %>%
+  add_lines(
+    x = ~scotland2$`Month end`,
+    y = ~scotland2$Value,
+    color = ~scotland2$Indicator
+  )
+lines1
+
+fig <- plot_ly(x = ~scotland1$`Month end`,
+               y = ~scotland1$Value,
+               type = 'scatter',
+               mode = 'lines',
+               line = list(color = phs_colors("phs-green")),
+               name = '52+ Weeks',
+               #fill = 'tozeroy',
+               stackgroup = 'one',
+               fillcolor = 'rgba(131, 187, 38, 0.25)'
+               )
+
+fig <- fig %>% add_trace(x = ~scotland2$`Month end`,
+                         y = ~scotland2$Value,
+                         line = list(color = phs_colors("phs-purple")),
+                         name = '104+ Weeks',
+                         #fill = 'tozeroy',
+                         fillcolor = 'rgba(63, 54, 133, 0.25)')
+fig <- fig %>% layout(xaxis = list(title = 'Month End'),
+                      yaxis = list(title = 'Patients Waiting'))
+
+fig
