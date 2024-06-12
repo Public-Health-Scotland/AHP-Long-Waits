7ddf626475bbdb4e025a5de827c9f8b181d27f4b
stewaw01 <stewart.wilson@phs.scot>
2024-06-12 15:50:04 +0100

    A tiny bit of progress after a day of experimenting

--- read_wrangle_data.R
+++ read_wrangle_data.R
@@ -30,12 +30,16 @@ data <- read_excel(here("data", "Waiting Monthly [New Time-Bands] Snapshot ---Ex
          "156+ weeks",
          "208+ weeks"))
 
-scotland1 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator %in% c("52+ weeks")) %>% 
+scotland1 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "52+ weeks") %>% 
   filter(Specialty == "All AHP MSK Specialties")
 
-scotland2 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator %in% c("104+ weeks")) %>% 
+scotland2 <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "104+ weeks") %>% 
   filter(Specialty == "All AHP MSK Specialties")
 
+physio <- data %>% filter(Specialty == "Physiotherapy") %>% filter(Indicator == "52+ weeks") %>% 
+  filter(`NHS Board` != "Scotland")
+
+# Lines ----
 lines1 <- plot_ly() %>%
   add_lines(
     x = ~scotland1$`Month end`,
@@ -51,7 +55,8 @@ lines1 <- lines1 %>%
   )
 lines1
 
-fig <- plot_ly(x = ~scotland1$`Month end`,
+# Area ----
+area <- plot_ly(x = ~scotland1$`Month end`,
                y = ~scotland1$Value,
                type = 'scatter',
                mode = 'lines',
@@ -62,13 +67,62 @@ fig <- plot_ly(x = ~scotland1$`Month end`,
                fillcolor = 'rgba(131, 187, 38, 0.25)'
                )
 
-fig <- fig %>% add_trace(x = ~scotland2$`Month end`,
+area <- area %>% add_trace(x = ~scotland2$`Month end`,
                          y = ~scotland2$Value,
                          line = list(color = phs_colors("phs-purple")),
                          name = '104+ Weeks',
                          #fill = 'tozeroy',
                          fillcolor = 'rgba(63, 54, 133, 0.25)')
-fig <- fig %>% layout(xaxis = list(title = 'Month End'),
+area <- area %>% layout(xaxis = list(title = 'Month End'),
                       yaxis = list(title = 'Patients Waiting'))
+area
 
-fig
+
+scotland_CP <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "52+ weeks") %>% 
+  filter(Specialty == "Chiropody/Podiatry")
+
+scotland_OT <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "52+ weeks") %>% 
+  filter(Specialty == "Occupational Therapy")
+
+scotland_OR <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "52+ weeks") %>% 
+  filter(Specialty == "Physiotherapy")
+
+scotland_PH <- data %>% filter(`NHS Board` == "Scotland") %>% filter(Indicator == "52+ weeks") %>% 
+  filter(Specialty == "Physiotherapy")
+
+area2 <- plot_ly(x = ~scotland_CP$`Month end`,
+                y = ~scotland_CP$Value,
+                type = 'scatter',
+                mode = 'lines',
+                line = list(color = phs_colors("phs-green")),
+                name = 'Chiropody/Podiatry',
+                stackgroup = 'one',
+                fillcolor = 'rgba(131, 187, 38, 0.5)')
+
+area2 <- area2 %>% add_trace(x = ~scotland_OT$`Month end`,
+                           y = ~scotland_OT$Value,
+                           line = list(color = phs_colors("phs-purple")),
+                           name = 'Occupational Therapy',
+                           fillcolor = 'rgba(63, 54, 133, 0.5)')
+
+area2 <- area2 %>% add_trace(x = ~scotland_OR$`Month end`,
+                             y = ~scotland_OR$Value,
+                             line = list(color = phs_colors("phs-rust")),
+                             name = 'Orthorics',
+                             fillcolor = 'rgba(199, 57, 24, 0.5)')
+
+area2 <- area2 %>% add_trace(x = ~scotland_PH$`Month end`,
+                             y = ~scotland_PH$Value,
+                             line = list(color = phs_colors("phs-teal")),
+                             name = 'Physiotherapy',
+                             fillcolor = 'rgba(30, 127, 132, 0.5)')
+
+area2 <- area2 %>% layout(xaxis = list(title = 'Month End'),
+                        yaxis = list(title = 'Patients Waiting'))
+
+area2
+
+
+
+
+
