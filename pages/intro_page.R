8d0ca2972c3cd0d74acc59d50a2c893424a158f0
stewaw01 <stewart.wilson@phs.scot>
2024-07-23 12:54:56 +0100

    Added text to intro tab

--- pages/intro_page.R
+++ pages/intro_page.R
@@ -6,7 +6,11 @@ output$intro_page_ui <-  renderUI({
 	     fluidRow(
 	       h1("AHP-MSK Long Waits"),
 	       h2("Notes"),
-	       p(""),
+	       p("This dashboard has been created to assist with conversations around long patient waits for AHP-MSK services."),
+	       p("The chart below shows the monthly distribution of patients waits from 25 weeks up to 52 weeks from March 2023 to March 2024."),
+	       p("The second tab contains two charts which provide alternative views of these figures.
+	         The first shows waits from 52+ weeks up to 208+ weeks, and can be be filtered by Board and Specialty.
+	         The second shows the breakdown of all 4 specialties and can be filtered by Board and Time Band."),
 	       
 	       plotlyOutput("intro_plot")
 	      ) #fluidrow
