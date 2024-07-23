577428e06c086a10fae7cb5eb98f9318d0856a4b
stewaw01 <stewart.wilson@phs.scot>
2024-07-23 13:59:07 +0100

    text update

--- pages/intro_page.R
+++ pages/intro_page.R
@@ -8,7 +8,7 @@ output$intro_page_ui <-  renderUI({
 	       h2("Notes"),
 	       p("This dashboard has been created to assist with conversations around long patient waits for AHP-MSK services."),
 	       p("The chart below shows the monthly distribution of patients waits from 25 weeks up to 52 weeks from March 2023 to March 2024."),
-	       p("The second tab contains two charts which provide alternative views of these figures.
+	       p("The Breakdown tab contains two charts which provide alternative views of these figures.
 	         The first shows waits from 52+ weeks up to 208+ weeks, and can be be filtered by Board and Specialty.
 	         The second shows the breakdown of all 4 specialties and can be filtered by Board and Time Band."),
 	       
